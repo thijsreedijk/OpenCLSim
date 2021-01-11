b19525ebcf8a2f60a019efcc548c0cb3b888072a
ThijsReedijk <thijsreedijk@gmail.com>
2021-01-11 16:06:39 +0100

    removed trailing comma

--- openclsim/appendix/__init__.py
+++ openclsim/appendix/__init__.py
@@ -1,5 +1,5 @@
 """Some extra functionality."""
 
-from .appendix import flatten, get_event_log, remove_item,
+from .appendix import flatten, get_event_log, remove_item
 
 __all__ = ["remove_item", "get_event_log", "flatten"]
